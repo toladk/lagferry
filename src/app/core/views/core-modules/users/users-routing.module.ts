85aabc2a5eb91d18e1c9d11302e17285fa572f12
MohBorLarge <shittu.mobolaji.7@gmail.com>
2022-11-22 11:49:56 +0100

    update user

--- src/app/core/views/core-modules/users/users-routing.module.ts
+++ src/app/core/views/core-modules/users/users-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'update-profile', component: UpdateProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
